64b4a93a037721717f0b23d0d5190c0da682d21e
David Benitez <debml700@gmail.com>
2016-04-12 15:26:01 -0500

    Added part 2

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 
 int main(){
      int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
+    int wt[] = {20, 10, 30};
     int  W = 50;
     int n = sizeof(val)/sizeof(val[0]);
     printf("%d", knapSack(W, wt, val, n));
