0ed11b3366ffebff80f44ad153785faf36a97bb9
David Benitez <debml700@gmail.com>
2016-04-12 15:26:45 -0500

    useless commit
    removed a space

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include "knapsack.h"
 
 int main(){
-     int val[] = {60, 100, 120};
+    int val[] = {60, 100, 120};
     int wt[] = {20, 10, 30};
     int  W = 50;
     int n = sizeof(val)/sizeof(val[0]);
